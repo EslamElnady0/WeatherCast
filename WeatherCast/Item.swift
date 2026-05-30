2b394de6ea7e345866374666f73af4ba3887cfad
EslamElnady0 <eslamelnady254@gmail.com>
2026-05-30 11:08:01 -0700

    init commet

---
+++ WeatherCast/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WeatherCast
+//
+//  Created by Eslam Elnady on 30/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
